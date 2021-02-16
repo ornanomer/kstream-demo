1d2c64c9eb8b8d1519e93ae5175b5e4b35f6cec9
omer ornan <oornan@paypal.com>
2021-02-16 14:31:11 +0200

    first commit

--- src/main/scala/exercise/sol/AggregateDrawPerAccountPer10MinSol.scala
+++ src/main/scala/exercise/sol/AggregateDrawPerAccountPer10MinSol.scala
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit
 
 import data.Transaction
 import exercise.KafkaTopics
-import exercise.KafkaTopics.WINDOW_SUM_TRAN
 import org.apache.kafka.common.serialization.{Serde, Serdes}
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.scala._
@@ -25,7 +24,7 @@ object AggregateDrawPerAccountPer10MinSol {
     implicit val transactionSerde: Serde[Transaction] = new TransactionSerde
 
     val config = new Properties
-    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ks") //consumer group
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ks-2") //consumer group
     config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092") // kafka broker
     config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
     config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, classOf[TransactionSerde])
@@ -38,7 +37,7 @@ object AggregateDrawPerAccountPer10MinSol {
       stream[String, Transaction](KafkaTopics.TRANSACTION_TOPIC).map[String, Double]((k,v) => (v.accountId, v.withDraw))
       .groupByKey.windowedBy(TimeWindows.of(TimeUnit.SECONDS.toMillis( 5 ) )).reduce((v1,v2) => v1 +v2).toStream
 
-    stream.foreach((k,v) => print( "start -> " + k.window().start() + "  key -> " + k.key() + " value ->" + v.toString() ))
+    stream.foreach((k,v) => println( "start -> " + k.window().start() + "  key -> " + k.key() + " value ->" + v.toString() ))
 
     val streams = new KafkaStreams(builder.build, config)
     streams.start()
